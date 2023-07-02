3cdae13c7f6be11fccab0bf0932ae0b85d05ce8c
careyesp95 <careyesp@libertadores.edu.co>
2023-07-02 14:22:54 -0500

    gateway service

---
+++ gateway/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const server = express();
+server.use(morgan('dev'));
+
+server.use('/characters', createProxyMiddleware({
+    target:'http://localhost:8001',
+    changeOrigin:true
+}))
+server.use('/films', createProxyMiddleware({
+    target:'http://localhost:8002',
+    changeOrigin:true
+}))
+server.use('/planets', createProxyMiddleware({
+    target:'http://localhost:8003',
+    changeOrigin:true
+}))
+
+const PORT_GATEWAY = 8000;
+
+server.listen(PORT_GATEWAY, () => {
+    console.log(`Gateway server listening on port ${PORT_GATEWAY}`);
+})
